43bb00137638a25e3df8e608d15c62344a1a4d00
Daniele Mancinelli <mancinelli.daniele@iisgalvanimi.edu.it>
2024-05-16 16:07:02 +0000

    Completato esercizio aggiunto precedentemente

--- regioni.py
+++ regioni.py
@@ -18,9 +18,12 @@ def search():
     return render_template('radiobutton.html', tabella = table)
 
 @app.route('/info', methods = ['GET'])
-def info()
-import pandas as pd
-    regione = request.args['id']
+def info():
+    import pandas as pd
+    indice = int(request.args['id'])
+    dati_regioni = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+    riga_selezionata = dati_regioni.iloc[[indice]] #la doppia parentesi serve per visualizzare il record come dataframe e non series
+    return riga_selezionata.to_html()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
